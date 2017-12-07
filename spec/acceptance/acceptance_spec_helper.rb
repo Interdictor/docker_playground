0246bfeee19fe797badba48a283386a3f969e2f1
Interdictor <interdictor.dsg@gmail.com>
2017-12-07 19:49:39 +0100

    Use Selenium container for acceptance tests

--- spec/acceptance/acceptance_spec_helper.rb
+++ spec/acceptance/acceptance_spec_helper.rb
@@ -2,13 +2,19 @@ require 'sinatra'
 require 'capybara'
 require 'capybara/rspec'
 require 'capybara/dsl'
+require 'selenium-webdriver'
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  Capybara::Selenium::Driver.new(app, {
+      browser: :remote,
+      url: 'http://selenium:4444/wd/hub',
+      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome
+    }
+  )
 end
 
 Capybara.default_driver = :chrome
-Capybara.app_host = "http://localhost:4567"
+Capybara.app_host = "http://web:4567"
 
 RSpec.configure do |config|
   config.warnings = false
