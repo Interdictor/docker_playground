92dffb681033ac7ac09b64e10cb32e3c5504f472
Interdictor <interdictor.dsg@gmail.com>
2017-12-08 15:37:06 +0100

    Rename Docker containers

--- spec/acceptance/acceptance_spec_helper.rb
+++ spec/acceptance/acceptance_spec_helper.rb
@@ -6,14 +6,14 @@ require 'selenium-webdriver'
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, {
-      url: 'http://selenium:4444/wd/hub',
+      url: 'http://selenium_container:4444/wd/hub',
       desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome
     }
   )
 end
 
 Capybara.default_driver = :chrome
-Capybara.app_host = "http://web:4567"
+Capybara.app_host = "http://sinatrilla_container:4567"
 
 RSpec.configure do |config|
   config.warnings = false
